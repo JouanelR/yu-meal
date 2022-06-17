03775b6e0a969d589fd28208d575292dd78b9254
Bardane <theo.martin@efrei.net>
2022-06-17 16:36:42 +0200

    Give_rec fonctionnel

--- scratch.py
+++ scratch.py
@@ -4,45 +4,40 @@ from sklearn.metrics.pairwise import sigmoid_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
 import time
 seconds = time.time()
-data = pd.read_csv("PP_recipes.csv")
-df = pd.read_csv("RAW_recipes.csv")
 
 
-data.shape
-df.shape
-
-df_merge = data.merge(df, on='id')
+df = pd.read_csv("Recipes Cleaned.csv")
 
 
-df_clean = df_merge.drop(columns=["i","name_tokens","ingredient_tokens","steps_tokens","techniques","ingredient_ids","contributor_id","submitted","steps","description"])
 
-df_2000 = df.head(10000) #echantillon de 20000 (pour l'instant) sinon trop lent
-#enlever les id likés et non likés
+
 
 
 
 tfv = TfidfVectorizer(min_df=3, max_features=None, strip_accents = 'unicode', analyzer = 'word', token_pattern=r'\w{1,}', ngram_range=(1,3), stop_words = 'english')
-tfv_matrix = tfv.fit_transform(df_2000['ingredients'])
-sig = sigmoid_kernel(tfv_matrix,tfv_matrix, gamma=.5)
+tfv_matrix = tfv.fit_transform(df['ingredients'])
+print("Seconds since epoch =", seconds-time.time())
 
+sig = sigmoid_kernel(tfv_matrix,tfv_matrix, gamma=.5)
+print("Seconds since epoch =", seconds-time.time())
+indices = pd.Series(df.index, index=df["name"]).drop_duplicates()
 
 def give_rec(recipe_id, sig=sig):
 
-    idx = (df_2000.index[df["id"] == recipe_id]).tolist()
-    print(idx[0])
+    idx = indices[recipe_id]
 
-    sig_scores = list(enumerate(sig[idx[0]]))
+    sig_scores = list(enumerate(sig[idx]))
     sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
-    sig_scores = sig_scores[1,21]
+    sig_scores = sig_scores[1:21]
 
     recipe_indices = [i[0] for i in sig_scores]
 
-    return df_2000["id"].iloc[recipe_indices]
+    return df["name"].iloc[recipe_indices]
 
 
 
-print("Seconds since epoch =", seconds-time.time())
-print(give_rec(46))
+
+print(give_rec(5789))
 print("Seconds since epoch give rec=", seconds-time.time())
 
 

984e9326516b9251cc9f64e5efaadac2c3b0a767
Mr-Corentin <72504878+Mr-Corentin@users.noreply.github.com>
2022-06-23 09:45:00 +0200

    coco

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');

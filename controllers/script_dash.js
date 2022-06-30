600cd4da1d45b8835ea5573a82db8d9762da9e18
Romain <romain.jouanel@efrei.net>
2022-06-30 15:38:09 +0200

    moins de console log

--- controllers/script_dash.js
+++ controllers/script_dash.js
@@ -15,10 +15,8 @@ var dejarecommende = 0;
 
 function sendDataCallback() {
     if (xhr.readyState == 4 && xhr.status == 201) {
-        console.log("Creation data received 1");
+        console.log("Creation data received :");
         dd = xhr.responseText
-        console.log("ici bg");
-        var oklm;
         JSON.parse(dd, (key, value) => {
             if (key == "message") {
                 lrepA = value;
@@ -47,20 +45,14 @@ exports.getDashboard = (req, res) => {
     xhr.onreadystatechange = sendDataCallback;
     
     // asynchronous requests
-    console.log("Sending request to server...");
     xhr.open("POST", "http://localhost:6969/dashboard", true);
-    console.log("Receving data...");
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     // Send the request over the network
     xhr.send(JSON.stringify({"data": dataToSend}));
-    console.log("Data sent!");
 }
 
 
 exports.recommendationHandle = (req, res) => {
-    console.log("1234567890123456")
-    console.log(nameA);
-    console.log(idA);
     myScript.getDashboard();
     var dmc_setInterval = setInterval(test,8000);
     var okk = 0;
@@ -71,29 +63,11 @@ exports.recommendationHandle = (req, res) => {
     
 }
 
-/*exports.recommendationHandle = (req, res) => {
-    const id2 = req.id2;
-    const idRecette = req.idRecette;
-    const name = req.name;
-    console.log(name);
-    idA = id2;
-    idRecetteA = idRecette;
-    console.log(idA);
-    myScript.getDashboard();
-    var dmc_setInterval = setInterval(test,8000);
-    function test () {
-        clearTimeout(dmc_setInterval);
-        res.render('dash',{
-            id : idA, name : name,  utils: myScript, donne: repA, 
-        });
-    }
-}*/
 //------------ User Model ------------//
 const Like = require('../models/likes');
 
 exports.likeHandle = (req, res) => {
     const { id2,idRecette } = req.body;
-    console.log(idRecette);
     const newLike = new Like({
         id_user :id2,
         id_recette : idRecette
@@ -113,8 +87,6 @@ exports.likeHandle = (req, res) => {
 const Dislike = require('../models/dislikes');
 exports.dislikeHandle = (req, res) => {
     const { name,id2,idRecette } = req.body;
-    console.log(id2)
-    console.log(idRecette);
     idA = id2;
     idRecetteA = idRecette;
     nameA = name;
@@ -139,6 +111,5 @@ exports.nextRepA = (req, res) => {
         myScript.recommendationHandle();
         dejarecommende = 0;
     }
-    repA = lrepA[dejarecommende]
-    console.log(repA,typeof repA);
+    repA = lrepA[dejarecommende];
 }
